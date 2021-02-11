873d1fc0c099b195285804548ba9e41cbf34b87f
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-02-10 18:20:44 -0800

    pseudocode written

---
+++ automation/automation.py
@@ -0,0 +1,26 @@
+import re
+
+# # phone = 'phone_numbers.txt'
+# # email = 'email.txt'
+
+# with open('phone_numbers.txt', 'a') as phone:
+#     with open('email.txt', 'a') as email:
+
+#         with open('potential-contacts.txt') as full_file:
+#             line = full_file.readlines()
+#             if line....(regex phone):
+#                 write to phone
+            
+#             elif line...(regex email):
+#                 write to email
+            
+#             else
+#                 next line
+                
+#         read 'email.txt' line and sort by first char
+#             until EOF
+#             close file
+#         read 'phone_numbers.txt' line and sort by first char
+#             until EOF
+#             close file
+            
